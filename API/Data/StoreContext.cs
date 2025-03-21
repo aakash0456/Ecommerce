8e1b2e353a4707a71b8c25cd65cbfd2c271ad1a7
akashsswhite <akashsharma@sswhite.net>
2025-03-21 10:52:05 -0400

    End of section 2

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+namespace API.Data;
+public class StoreContext(DbContextOptions options) : DbContext(options)
+{
+
+    public required DbSet<Product> Products{ get; set; }
+}
