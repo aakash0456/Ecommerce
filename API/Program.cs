3422305447e1ce26eb3869c13b03916988155ed3
aakash0456 <aakashsharma0456@gmail.com>
2025-05-27 16:22:31 -0400

    final end of section 11

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<StoreContext>(opt => 
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDbContext<StoreContext> (opt =>
+  {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+  }
+);
 builder.Services.AddCors();
 builder.Services.AddTransient<ExceptionMiddleware>();
 builder.Services.AddScoped<PaymentsService>();
